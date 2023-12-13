8eae553b89e1bdd49312ce1040205527d69eab64
Chebs Awah <chebesiawah@gmail.com>
2023-12-13 11:10:07 +0100

    search bar and hero carousel

---
+++ components/HeroCarousel.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+const heroImages = [
+  {imageUrl:"/assets/images/hero-1.svg",alt:"smart watch"},
+  {imageUrl:"/assets/images/hero-2.svg",alt:"bag"},
+  {imageUrl:"/assets/images/hero-3.svg",alt:"lamp"},
+  {imageUrl:"/assets/images/hero-4.svg",alt:"air fryer"},
+  {imageUrl:"/assets/images/hero-5.svg",alt:"chair"},
+]
+const HeroCarousel = () => {
+  return (
+    <div className="hero-carousel">
+      <Carousel
+      showThumbs={false}
+      autoPlay
+      infiniteLoop
+      interval={2000}
+      showArrows={false}
+      showStatus={false}
+      >
+        {
+          heroImages.map((img) => (
+        
+         <Image src={img.imageUrl} 
+         alt={img.alt} 
+         key={img.alt}
+         width={484}
+         height={484}
+         className="object-contain"
+         />
+         
+          ))
+        }
+      </Carousel>
+    </div>
+  );
+};
+
+export default HeroCarousel;
